00b7befc30d17f3c77e5d47e1d09cfcc0da61359
Neeraj Tk <neerajtk560@gmail.com>
2020-03-18 11:25:09 +0530

    Add files via upload

---
+++ JS/Age Calculator/script.js
@@ -0,0 +1,2 @@
+var age = prompt("Enter age: ");
+console.log(age ," years roughly equals ", age*365.25 , " days.");
---
+++ JS/Stalker/script.js
@@ -0,0 +1,6 @@
+var fname = prompt("Enter first name: ");
+var lname = prompt("Enter last name: ");
+var age = prompt("Enter age: ");
+alert("Welcome to the website , "+fname ," ", +lname);
+alert("Your current age is "+age);
+console.log(fname," logged in suucessfully");
