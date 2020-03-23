1cb30660bcd6bc741f738c25c3903fb4678e97eb
Neeraj Tk <neerajtk560@gmail.com>
2020-03-23 14:17:21 +0530

    Add files via upload

---
+++ JS/DOM/ColorChange DOM/script.js
@@ -0,0 +1,27 @@
+var button = document.querySelector("button");
+var isPurple=false;
+button.addEventListener("click",function(){
+    if(isPurple)
+    {
+        document.body.style.background="white";
+        // isPurple=false;
+
+    }
+    else 
+    {
+        document.body.style.background="purple";
+        // isPurple=true;
+        
+    }
+    isPurple = !isPurple;
+}
+);
+
+// OR  
+
+// button.addEventListener("click", function(){
+//     document.body.classList.toggle("purple");
+//     }
+
+
+// );
